9509812735e6ffb6c471c0b57dc24db0d5cb0cbf
gkavin57 <gkavin77@gmail.com>
2023-06-14 12:47:25 +0530

    connect

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark t p-4">
+      <h6 className="text-center text-white">
+        All rights reserved &copy; Gayathiri
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
